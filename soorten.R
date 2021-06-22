f9c3387f9e487a188a0fbce38d032096c1237be8
DaanG <daan.gerla@anteagroup.com>
2021-06-22 17:25:14 +0200

    Just commit

--- soorten.R
+++ soorten.R
@@ -2,5 +2,11 @@ library('readxl')
 
 telling <- read.csv('data/NDFF-export_26-05-2021_15-51-55.csv', stringsAsFactors=F)
 
-typische_soorten <- read_excel('data/typische_soorten.xlsx')
+typsoort <- read_excel('data/typische_soorten.xlsx')
 
+unique(telling$soort_ned)
+qnique(telling$soort_wet)
+typsoort[(typsoort[['Wetenschappelijke naam']] %in% telling$soort_wet),]
+
+table(telling$loc_type)
+head(telling, 1)
